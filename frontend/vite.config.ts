3fb824bb2edf3c393d3778f0a3d744aef54738ad
caner <canercetin853@gmail.com>
2024-10-28 20:17:50 +0300

    build output fix and screenshots for readme

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,8 +5,4 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), TanStackRouterVite()],
-	build: {
-		outDir: "./out",
-		emptyOutDir: true,
-	},
 });
